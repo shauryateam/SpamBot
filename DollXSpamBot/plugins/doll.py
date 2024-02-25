e0764129e2c3aa6a8946a6c1142ad70b5148926c
SHAURYA_X_TEAM <100112352+shauryateam@users.noreply.github.com>
2024-02-25 11:06:43 +0530

    Update doll.py

--- DollXSpamBot/plugins/doll.py
+++ DollXSpamBot/plugins/doll.py
@@ -15,17 +15,17 @@ from telethon.tl.custom import button
 from time import time
 from datetime import datetime
 
-DOLL_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/f8d63b1dc5676fc9988f1.jpg"
+DOLL_PIC = ALIVE_PIC if ALIVE_PIC else "https://graph.org/file/32e883a07287e1291a8ab.jpg"
 
 
-DOLL = "✯ 𝘿𝙤𝙡𝙡 ✘ 𝙎𝙥𝙖𝙢 𝙃𝙀𝙍𝙀 ✯\n\n"
+DOLL = "✯ 𝙎𝙝𝙖𝙪𝙧𝙮𝙖 ✘ 𝙎𝙥𝙖𝙢 𝙃𝙀𝙍𝙀 ✯\n\n"
 DOLL += f"**꧁🇮🇳 🎀  𝑀𝒶𝒹𝑒 𝐼𝓃 𝐼𝓃𝒹𝒾𝒶  🎀 🇮🇳꧂**\n"
 DOLL += f"━───────╯•╰───────━\n"
 DOLL += f"• **𝙿𝚈𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽** : `3.10.1`\n"
 DOLL += f"• **𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽** : `{version.__version__}`\n"
 DOLL += f"• **𝙳𝙾𝙻𝙻 𝚇 𝚂𝙿𝙰𝙼 𝙱𝙾𝚃 𝚅𝙴𝚁𝚂𝙸𝙾𝙽**  : `{deadlyversion}`\n"
-DOLL += f"• **ᴄʜᴀɴɴᴇʟ** : [Join.](https://t.me/DollxSpam_BOT)\n"
-DOLL += f"• **Source Code** : [•Repo•](https://github.com/DOMINATOR-XD/DollXSpamBot)\n"
+DOLL += f"• **ᴄʜᴀɴɴᴇʟ** : [Join.](https://t.me/hey_villa)\n"
+DOLL += f"• **Source Code** : [•Repo•](https://t.me/hey_villa)\n"
 DOLL += f"━───────╮•╭───────━\n\n"   
                                   
 @BOT0.on(events.NewMessage(incoming=True, pattern=r"\%sdoll(?: |$)(.*)" % hl))
@@ -36,11 +36,11 @@ async def alive(event):
                                   caption=DOLL,
                                   buttons=[
         [
-        Button.url("☺️ᴄʜᴀɴɴᴇʟ☺️", "https://t.me/Dollx_spambot"),
+        Button.url("☺️ᴄʜᴀɴɴᴇʟ☺️", "https://t.me/shauryateam/spambot"),
         Button.url("🇮🇳sᴜᴘᴘᴏʀᴛ🇮🇳", "https://t.me/DollxSpam_BOT")
         ],
         [
-        Button.url("• 🙂ʀᴇᴘᴏ🙂 •", "https://github.com/DOMINATOR-XD/DollXSpamBot")
+        Button.url("• 🙂ʀᴇᴘᴏ🙂 •", "https://github.com/shauryateam/spambot")
         ]
         ]
         )
